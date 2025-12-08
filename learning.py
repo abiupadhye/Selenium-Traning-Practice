8d4741a1e02b3db988b87022a5193b65a922e09a
abiupadhye <abhishekupadhye862@gmail.com>
2025-12-08 11:54:39 +0530

    defined even odd

--- learning.py
+++ learning.py
@@ -1,6 +1,9 @@
-num = int(input("Enter a number: "))
+def even_odd(start, end):
+    for num in range (10,20):
 
-if num%2==0:
-    print(f"{num} is even")
-else:
-    print(f"{num} is odd")
+     if num%2==0:
+        print(f"{num} is even")
+    else:
+        print(f"{num} is odd")
+
+even_odd(100,200)
