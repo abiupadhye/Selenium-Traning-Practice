b94e2986345903eba7be049e4f7f8b0ef36c6160
abiupadhye <abhishekupadhye862@gmail.com>
2025-12-09 11:23:06 +0530

    Added Class

--- learning1.py
+++ learning1.py
@@ -1,6 +1,9 @@
-num = 10
+def even_odd(start, end):
+    for num in range (10,20):
 
-if num%2==0:
-    print(f"{num} is even")
-else:
-    print(f"{num} is odd")
+     if num%2==0:
+        print(f"{num} is even")
+    else:
+        print(f"{num} is odd")
+
+even_odd(100,300)
